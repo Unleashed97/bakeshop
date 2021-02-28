6d11eef2960e10ab05cb7d1556d6ffd5407d6072
Nichkov Roman <nichkovroman@gmail.com>
2021-02-28 23:04:45 +0300

    location fix. for header on github

--- src/js/header-top.js
+++ src/js/header-top.js
@@ -1,14 +1,11 @@
 const headerTop = () =>{
     const header = document.querySelector('.header');
     let is_root = location.pathname == "/";
-    console.log(location.pathname);
-    
-    // let loc = window.location.pathname;
 
     if(!is_root && location.pathname !== '/index.html'){
         header.classList.add('scroll');
     }
-    else if(is_root || location.pathname == '/index.html'){
+    else if(is_root || location.pathname == '/index.html' || location.pathname == '/bakeshop/dist/index.html'){
         const intro = document.querySelector('.intro');
         let introHeight = intro.scrollHeight;
         window.addEventListener('scroll', () =>{
